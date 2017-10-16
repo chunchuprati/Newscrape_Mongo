a4282f8971e2cb6e761a2ab480d4409aa30f49b4
prathibha <prathibha.panga@gmail.com>
2017-10-16 10:25:57 -0700

    all files created

---
+++ server.js
@@ -0,0 +1,32 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var apiRoutes = require("./server/routing/apiRoutes")
+var htmlRoutes = require("./server/routing/htmlRoutes");
+var cheerio = require('cheerio')
+var port = process.env.PORT || 3000;
+// Requiring our models for syncing
+var db = require("./models");
+var app = express();
+
+
+
+app.use(express.static("public"));
+app.use(methodOverride("_method"));
+app.use(bodyParser.json()) // For ajax
+app.use(bodyParser.urlencoded({extended: false})) // For html forms
+app.use(session({ secret: "THIS IS A SECRET", resave: false, saveUninitialized: false }))
+app.use(passport.initialize())
+app.use(passport.session())
+require('./server/passport')(passport);
+
+app.use(apiRoutes);
+app.use(htmlRoutes);
+app.use(account);
+
+db.sequelize.sync().then(function() {
+    app.listen(port, function() {
+      console.log("App listening on PORT " + port);
+    });
+  });
+  
